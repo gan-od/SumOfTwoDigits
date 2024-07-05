d4918bc48ee3b43438fe3265fb4e3b8055bb489c
Gan-Od Bayarbaatar <gb@Gan-Ods-MacBook-Air.local>
2024-07-05 02:00:33 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        //java.util.Scanner s = new java.util.Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = a + b;
+        System.out.println(c);
+    }
+}
